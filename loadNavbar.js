5e289387fc3853b37c266380b83ffab4e9765053
Ayakaze <yzwralph@gmail.com>
2024-07-03 16:04:47 +0800

    Update loadNavbar.js

--- loadNavbar.js
+++ loadNavbar.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-placeholder').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading the navbar:', error));
+    const navbarPlaceholder = document.getElementById('navbar-placeholder');
+    if (navbarPlaceholder) {
+        fetch('/navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                navbarPlaceholder.innerHTML = data;
+            })
+            .catch(error => console.error('Error loading the navbar:', error));
+    }
 });
